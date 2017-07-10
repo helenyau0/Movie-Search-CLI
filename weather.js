c85701688ffbb6f1403f20df496b87660a1bc09a
Helen Yau <helenyau13@gmail.com>
2017-07-10 14:33:25 -0700

    rename variable to be more specific to what it is

--- weather.js
+++ weather.js
@@ -5,10 +5,10 @@ function getWeather(city) {
     host: 'api.openweathermap.org',
     path: `/data/2.5/weather?q=${city}&mode=json&appid=2931fa08fcb2950740aef2103b931306`,
   }, (res) => {
-    let html = ''
-    res.on('data', (chunk) => { html += chunk })
+    let rawData = ''
+    res.on('data', (chunk) => { rawData += chunk })
     res.on('end', () => {
-      let json = JSON.parse(html)
+      let json = JSON.parse(rawData)
       let temperature = Math.floor((((json['main']['temp'] * 9)/ 5) - 459.67))
       console.log('Temperature in Farenheit: ' + temperature);
     })
@@ -16,7 +16,7 @@ function getWeather(city) {
 }
 
 function Whats_the_weather() {
-  const city = process.argv.splice(2).join('+')
+  const city = process.argv.slice(2).join('+')
   getWeather(city)
 }
 
